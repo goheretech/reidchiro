0a13175013f1e36a41dfbeecb0e433d102c0c22b
jacklemay <clemay.lm@gmail.com>
2020-01-24 00:27:23 -0600

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,11 +43,10 @@
 	<section style="background-image:url(img/reid2.jpg)" class="w-full h-full bg-center bg-cover">
 		<div class="w-full h-full flex items-center justify-center text-white flex-col relative" style="background:rgba(30,30,30,.30)">
 			<h2 class="px-128 text-center font-bold leading-none" style="font-family: 'Playfair Display', serif; font-size:90px;">Exceptional chiropractic care,<br> designed for real life</h2>
-			<a href="#" class="mt-12 bg-green-700 gold rounded-full px-8 py-4">Sign Up today</a>
-			<h3 class="font-bold mt-6">Lorem ipsum dolor sit amet, consectetur</h3>
+			
 			<div class="mb-24 absolute bottom-0 font-bold flex flex-col items-center justify-center">
-				Scroll Down
-				<div class="mt-4 border-white w-8 h-8 border-l-4 border-b-4" style="transform:rotate(-45deg)"></div>
+				<a href="#" class="mt-12 bg-green-700 gold rounded-full px-8 py-4">Sign Up today</a>
+				<h3 class="font-bold mt-6">Lorem ipsum dolor sit amet, consectetur</h3>
 			</div> 
 		</div>
 	</section>

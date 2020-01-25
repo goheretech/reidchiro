c622bf7bf6f7b9a38179d89fa52265ca0b3d757a
jacklemay <clemay.lm@gmail.com>
2020-01-24 23:11:19 -0600

    gogo

--- src/routes/index copy.svelte
+++ src/routes/index copy.svelte
@@ -40,22 +40,29 @@
 </svelte:head>
 <NavSlim />
 <div class="flex items-center bg-gray-100 h-screen">
-	<section style="background-image:url(img/kitch2.jpg)" class="w-full h-full bg-center bg-cover">
-		<div class="w-full h-full flex items-center justify-center flex-col relative" style="background:rgba(30,30,30,.20)">
-			<container class="flex justify-end items-end text-center h-full" style="width:1300px;">
-				<div class="bg-gray-100 w-1/2 px-12 py-32">
-					<h2 style="font-family: 'Playfair Display'; font-size: 48px;">"My goal is to educate and empower patients to live a healthy optimized life."</h2>
-					<div class="flex justify-center items-center mt-12 pt-12 border-solid border-t-4 border-gray-300">
-						<div class="rounded-full bg-center bg-cover mr-8 " style="background-image:url(img/int/_OSR6163.jpg); width:80px; height:80px;"></div>
-						<h3 style="font-size:40px; font-family: 'Yellowtail', cursive;">Charles Reid M.D.</h3>
-					</div>
-				</div>
+	<section style="background-image:url(img/reid2.jpg)" class="w-full h-full bg-center bg-cover">
+		<div class="w-full h-full flex items-center justify-center text-white flex-col relative" style="background:rgba(30,30,30,.30)">
+			<container class="flex justify-center items-center flex-col text-center" style="width:1300px;">
+			<h2 class="text-center font-bold leading-none" style="font-family: 'Playfair Display', serif; font-size:90px;">Put your Health in Good Hands</h2>
+			
+			<div class="mb-24 absolute bottom-0 font-bold flex flex-col items-center justify-center">
+				<a href="#" class="mt-12 bg-green-700 gold rounded-full px-8 py-4">Sign Up today</a>
+				<h3 class="font-bold mt-6">Personal, proactive, life-changing care</h3>
+			</div> 
 			</container>
 		</div>
 	</section>
 </div>
+<section  class="flex justify-center items-center flex-col py-48">
+	<container class="flex justify-center items-center flex-col text-center" style="width:1100px;">
+		<h2 class="text-green-800 font-semibold leading-none mb-10" style="font-family: 'Playfair Display', serif; font-size:75px;">We’re educating and empowering patients</h2>
+		<p class="text-lg text-green-900">
+			Start living a healthy, optimized life. Rather than do things the typical way, we’ve created a membership-based primary care practice truly designed around your needs. From same-day appointments that start on time to compassionate providers who listen to 24/7 video visits, we make it faster, easier, and more enjoyable to get care — whenever and wherever you need it.
+		</p>
+	</container>
+</section>
 {#each homeBlurb as blurb, i}
-<section class="flex mx-32 mt-48" class:flex-row-reverse={i%2!=0}>
+<section class="flex mx-32 mb-48" class:flex-row-reverse={i%2!=0}>
 	<div style="background-image:url({blurb.img})" class="bg-center bg-cover w-1/2 "></div>
 	<div class="w-1/2" class:pl-32={i%2==0} class:my-64={i%2!=0} class:my-32={i%2==0} class:pr-32={i%2!=0}>
 		<h2 class="text-green-800 font-semibold leading-none mb-10" style="font-family: 'Playfair Display', serif; font-size:70px;">
@@ -83,7 +90,7 @@
 			</section> 
 -->
 
-<section style="font-family: 'Playfair Display', serif;" class="bg-green-700 p-32 mt-48">
+<section style="font-family: 'Playfair Display', serif;" class="bg-green-700 p-32">
 	<h3 class="text-white mb-12 text-xl">How we can help:</h3>
 	{#each services as service, i}
 		<h2 class="font-bold text-white leading-none mb-4 " style="font-family: 'Playfair Display', serif; font-size:110px;"><a class="service-item" href="{service.url}">{service.name}</a></h2>
